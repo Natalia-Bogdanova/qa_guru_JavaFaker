c85efb25e8f687362c04f36ae9a7f82660137e31
Natalia-Bogdanova <bogdanowa.vlg@gmail.com>
2022-10-07 22:37:59 +0300

    Initial commit

---
+++ src/test/java/com/bogdanova/tests/TestPageObjectsTestMiniForm.java
@@ -0,0 +1,34 @@
+package com.bogdanova.tests;
+
+import com.bogdanova.pages.RegistrationFormPage;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestPageObjectsTestMiniForm {
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    @BeforeAll
+    static void configure(){
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormMiniTest() {
+        registrationFormPage.openPage()
+                .setFirstName("Vadim")
+                .setLastName("Pirogkov")
+                .setEmail("Pirogkov@gmail.com")
+                .setGender("Other")
+                .setuserNumber("9051112233")
+                .clickSubmit();
+
+        registrationFormPage.checkResultsTableVisible()
+                .checkResult("Student Name", "Vadim Pirogkov")
+                .checkResult("Student Email", "Pirogkov@gmail.com")
+                .checkResult("Gender", "Other")
+                .checkResult("Mobile", "9051112233")
+                .clickClose();
+    }
+}
